92ca7300b784161cab51ec22a588cdc4d2ec6e2b
ignacio mateluna <ignaciiio.mate@gmail.com>
2026-04-06 20:06:17 +0000

    "Por favor, abre una terminal y ejecuta estos comandos de configuración

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.clubpatio.app',
+  appName: 'Club Patio',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
